8710ef6814e72a68b90eff21807a1950d6ff693c
Mohammad Atif Aftab <iamatif96@gmail.com>
2020-10-15 18:49:15 +0800

    Spring boot CRUD with JUnit

---
+++ studentdal/src/main/java/com/atif/studentProject/dal/StudentdalApplication.java
@@ -0,0 +1,13 @@
+package com.atif.studentProject.dal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentdalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentdalApplication.class, args);
+	}
+
+}
